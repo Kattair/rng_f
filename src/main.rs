410333a251c7dd6d760ed745acbbb931026a2020
Martin Kustra <martin.kustra@aardwark.com>
2021-05-06 16:24:58 +0200

    Moved Config logic to its own module

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,14 @@
-use std::env;
+use std::{env, process};
 use std::error::Error;
+use std::fs::OpenOptions;
+use std::io::{BufWriter, Write};
+use std::path::Path;
 
-#[derive(Debug)]
-struct Config {
-    row_count: usize,
-    col_count: usize,
-    spaces: bool,
-    output_filename: String,
-}
+use rand::Rng;
 
-impl Config {
-    fn new(row_count: usize, col_count: usize, spaces: bool, output_filename: &str) -> Config {
-        let output_filename = String::from(output_filename);
+use config::Config;
 
-        Config {row_count, col_count, spaces, output_filename}
-    }
-
-    /*
-        Desired format:
-            rng_f rows_count cols_count [OPTION]
-
-            OPTION
-            -s, --spaces
-                generated space between columns for better user readability
-            -o, --output
-                specify output_filename, default is output.txt
-     */
-    fn parse_config(args: Vec<String>) -> Result<Config, Box<dyn Error>> {
-        if args.len() < 3 {
-            panic!("Not enough arguments.");
-        }
-
-        let row_count = args[1].parse()?;
-        let col_count = args[2].parse()?;
-        let mut spaces = false;
-        let mut output_filename = String::from("output.txt");
-
-        if args.len() < 4 {
-            return Ok(Config {row_count, col_count, spaces, output_filename});
-        }
-
-        for i in 3..(args.len()) {
-            let arg = &args[i];
-            if "-s".eq(arg) || "--spaces".eq(arg) {
-                spaces = true;
-            }
-            else if "-o".eq(arg) || "--output".eq(arg) {
-                output_filename = args[i + 1].to_string();
-            }
-        }
+mod config;
 
         Ok(Config {row_count, col_count, spaces, output_filename})
     }
