47e141332ce9f8024fa12a3644f4f43833832d3c
Martin Kustra <martin.kustra@tutanota.com>
2023-01-28 19:57:21 +0100

    refactor to include new clap changes

--- src/config/struct.rs
+++ src/config/struct.rs
@@ -1,22 +1,22 @@
-use clap::{crate_name, Parser};
+use clap::Parser;
 
 #[derive(Parser, Clone, Debug)]
-#[clap(name = crate_name!(), author, about, version)]
+#[command(author, about, version)]
 pub struct Config {
     /// Specify the number of rows
-    #[clap(required = true)]
     pub row_count: u128,
     /// Specify the number of columns
-    #[clap(required = true)]
     pub col_count: u128,
 
     /// Define a string to be used as the delimiter between columns
-    #[clap(short, long, default_value = " ", display_order = 1)]
+    #[arg(short,
+        long,
+        default_value = " ",
+        display_order = 1)]
     pub delimiter: String,
 
     /// Specify output filename
-    #[clap(
-        short,
+    #[arg(short,
         long,
         value_name = "FILENAME",
         default_value = "output.txt",
@@ -25,6 +25,10 @@ pub struct Config {
     pub output_filename: String,
 
     /// Specify bottom and top limit for number range. Creates interval <from, to)
-    #[clap(short, long, allow_hyphen_values = true, value_names = &["FROM", "TO"], display_order = 3)]
+    #[arg(short,
+        long,
+        allow_hyphen_values = true,
+        value_names = &["FROM", "TO"],
+        display_order = 3)]
     pub range: Option<Vec<i128>>,
 }
