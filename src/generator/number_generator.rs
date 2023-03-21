4f0adae12197160182e7799c92aa04beb59274a5
Martin Kustra <martin.kustra@tutanota.com>
2023-03-21 16:43:27 +0100

    refactor: replace range with uniform

--- src/generator/number_generator.rs
+++ src/generator/number_generator.rs
@@ -1,8 +1,10 @@
 use std::i128;
 use std::ops::Range;
 
+use rand::distributions::Uniform;
+use rand::prelude::Distribution;
 use rand::rngs::SmallRng;
-use rand::{Rng, SeedableRng};
+use rand::SeedableRng;
 
 use super::constants::{EMPTY_STRING, NEW_LINE};
 use super::Generator;
@@ -10,17 +12,17 @@ use super::Generator;
 pub struct NumberGenerator {
     buffer: String,
     rng: SmallRng,
-    range: Range<i128>,
+    uniform: Uniform<i128>,
     column_delimiter: String,
 }
 
 impl NumberGenerator {
     pub fn new(range: Range<i128>, column_delimiter: &str) -> Self {
         NumberGenerator {
-            buffer: String::with_capacity(32),
+            buffer: String::new(),
             rng: rand::rngs::SmallRng::from_entropy(),
             column_delimiter: column_delimiter.to_owned(),
-            range,
+            uniform: Uniform::from(range),
         }
     }
 }
@@ -35,11 +37,9 @@ impl Generator for NumberGenerator {
     }
 
     fn supply_element(&mut self) -> &str {
-        self.buffer.clear();
-        
-        let element = self.rng.gen_range(self.range.clone());
-        self.buffer.push_str(&element.to_string());
-        
+        let element = self.uniform.sample(&mut self.rng);
+        self.buffer = element.to_string();
+
         &self.buffer
     }
 
